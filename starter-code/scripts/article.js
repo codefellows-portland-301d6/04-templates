753730386bee09905a4944189590ee972348925e
nsmeds <niilosmeds@gmail.com>
2016-09-22 13:16:19 -0700

    moved return.html line to bottom of function in order to render handlebars template. fixed typo in index.html

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -17,7 +17,6 @@ Article.prototype.toHtml = function() {
   var template = Handlebars.compile(source);
   var html = template(this);
 
-  return html;
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced
@@ -28,6 +27,7 @@ Article.prototype.toHtml = function() {
 
   // TODO: Use the function that Handlebars gave you to return your filled-in
   //       html template for THIS article.
+  return html;
 };
 
 ourLocalData.sort(function(a,b) {
