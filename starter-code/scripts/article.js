29198c965d0cfc5831da70a613c397833383564e
Nathan Hugon <nthugon@gmail.com>
2016-09-22 13:25:39 -0700

    created method to populate and publish template to html

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -13,6 +13,10 @@ Article.prototype.toHtml = function() {
   // TODO: Use handlebars to render your articles!
   //       - Select your template from the DOM.
   //       - Now "compile" your template with Handlebars.
+  var source = $('#article-template').html();
+  var template = Handlebars.compile(source);
+  var html = template(this);
+  return html;
 
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
